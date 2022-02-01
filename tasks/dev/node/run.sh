1bb907a441a0748d9e05067fb4e4540d9f025f92
Hyungsuk Yu <4725435+hy2219@users.noreply.github.com>
2022-02-01 16:13:51 +0900

    Fixed script to not append nvm related lines that already exist in zshrc

--- tasks/dev/node/run.sh
+++ tasks/dev/node/run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 TASK_HOME=$(dirname "$0")
-source "$UTILS_DIR"/selectable_install.sh
 
 brew bundle --file "$TASK_HOME"/Brewfile --no-lock
 
 mkdir -p "$HOME"/.nvm
-printf "%s\n%s\n%s\n" "export NVM_DIR=\"$HOME/.nvm\"" "[ -s \"/usr/local/opt/nvm/nvm.sh\" ] && \. \"/usr/local/opt/nvm/nvm.sh\"" "[ -s \"/usr/local/opt/nvm/etc/bash_completion.d/nvm\" ] && \. \"/usr/local/opt/nvm/etc/bash_completion.d/nvm\"" >> "$HOME"/.zshrc
+COMMANDS=(
+    "export NVM_DIR=\"$HOME/.nvm\""
+    "[ -s \"/usr/local/opt/nvm/nvm.sh\" ] && . \"/usr/local/opt/nvm/nvm.sh\""
+    "[ -s \"/usr/local/opt/nvm/etc/bash_completion.d/nvm\" ] && . \"/usr/local/opt/nvm/etc/bash_completion.d/nvm\""
+)
+
+for cmd in "${COMMANDS[@]}"
+do
+    sed -i "" /"${cmd//\//\\/}"/d "$HOME"/.zshrc
+    echo "$cmd" >> "$HOME"/.zshrc
+done
